095306ab13fa3651f9f53ec2c4fa07e02d686c2e
Johnathan Farmer <98709695+ECGLABS@users.noreply.github.com>
2025-05-01 15:31:50 +0000

    Update Crypto Dice Roller.py

--- Crypto Dice Roller.py
+++ Crypto Dice Roller.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 #tkinter window generation code
 window = tk.Tk()
@@ -9,8 +10,25 @@ label.pack(pady=20)
 
 def roll_crypto():
     label.config(text="Rolling... (we'll add results soon)")
-    button = tk.Button(window, text="COME TIME TO GAMBA", command=roll_crypto)
+    button = tk.Button(window, text="TIME TO GAMBA", command=roll_crypto)
     button.pack()
+
+#Define Crypto List with a dictionary
+cryptos ={
+    1:"Bitcoin",
+    2:"Ethereum",
+    3:"Ripple",
+    4:"Dogecoin",
+    5:"Hex",
+    6:"Pulsechain" ,
+    }
+
+def roll_crypto2():
+    roll = random.randint (1, 6)
+    chosen = cryptos[roll]
+    label.config(text=f"You Rolled for {roll}!\nCrypto: {chosen}")
+
+
 
 
 window.mainloop()
